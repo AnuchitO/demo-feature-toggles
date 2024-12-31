a58b4259c0572887a2a0516f85d9482c738d8db7
Nong AnuchitO <anuchito.code@gmail.com>
2024-12-31 21:32:46 +0700

    note vite get process env

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // global variable process.env
+  // https://vitejs.dev/guide/env-and-mode.html#env-variables
 })
