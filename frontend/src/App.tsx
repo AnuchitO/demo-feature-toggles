19e2dbb3efd01911555f173ddb72479884a7df5e
Nong AnuchitO <anuchito.code@gmail.com>
2025-01-16 17:38:45 +0700

    extract Button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,9 @@
 import { Accounts } from './Accounts'
 import { MonthlyDropdown } from './MonthlyDropdown'
 import "./App.css"
-import icon from './icon-transfer.svg'
-import { makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
+import { TransferButton } from './TransferButton'
 import { commaSeparated } from './formater'
-
-const useStyles = makeStyles({
-  root: {
-    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-    border: 0,
-    borderRadius: 100,
-    paddingLeft: 32,
-    paddingRight: 32,
-    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-    color: 'white',
-    height: 36,
-    padding: '0 30px',
-    textTransform: 'none',
-  },
-});
+import { ScheduleButton } from './ScheduleButton'
 
 interface TransactionProps {
   type: string;
@@ -44,8 +28,6 @@ const Transaction = ({ type = "Activity", date = "18 Jan 2025 15:03", amount = 0
 
 
 const App = () => {
-  const classes = useStyles();
-
   const onClick = () => {
     alert('transfer')
   }
@@ -59,10 +41,8 @@ const App = () => {
             <Accounts />
           </div>
           <div>
-            <Button className={classes.root}
-              startIcon={<img src={icon} alt="transfer" className="w-6 h-6" />}
-              onClick={onClick}
-            > Transfer</Button>
+            <TransferButton onClick={onClick} />
+            <ScheduleButton onClick={onClick} />
           </div>
         </div>
         <div className="flex flex-col justify-center m-4" >
---
+++ frontend/src/TransferButton.tsx
@@ -0,0 +1,35 @@
+import icon from './icon-transfer.svg'
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles({
+  root: {
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    border: 0,
+    borderRadius: 100,
+    marginLeft: 8,
+    marginRight: 8,
+    paddingLeft: 32,
+    paddingRight: 32,
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    color: 'white',
+    height: 36,
+    padding: '0 30px',
+    textTransform: 'none',
+  },
+});
+
+interface Props {
+  onClick: () => void;
+}
+
+
+export const TransferButton = ({ onClick }: Props) => {
+  const classes = useStyles();
+  return <>
+    <Button className={classes.root}
+      startIcon={<img src={icon} alt="transfer" className="w-6 h-6" />}
+      onClick={onClick}
+    > Transfer</Button>
+  </>
+}
