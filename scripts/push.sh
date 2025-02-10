e4f57b4614efae36c8a8f650933bef49b8646485
AnuchitO <anuchito.code@gmail.com>
2025-02-10 20:27:26 +0700

    update push script

---
+++ scripts/push.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -euo pipefail
+
+echo "Pushing to all remotes..."
+
+for remote in $(git remote); do
+    git pull -r $remote main
+done
+
+for remote in $(git remote); do
+    git push $remote
+done
+
+echo "Pushed to all remotes."
+
