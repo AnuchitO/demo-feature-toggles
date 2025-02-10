8a4fccb53a2e4add6d719ccbdbeb743d6c1b4cfd
AnuchitO <anuchito.code@gmail.com>
2025-02-10 20:31:14 +0700

    test push script

--- scripts/push.sh
+++ scripts/push.sh
@@ -2,17 +2,16 @@
 
 set -euo pipefail
 
-printf "\360\237\215\272\t push to all remotes...  \n"
+printf "\360\237\215\272\t  Pulling from all remotes...  \n"
 
-# echo "Pushing to all remotes..."
-#
-# for remote in $(git remote); do
-#     git pull -r $remote main
-# done
-#
+for remote in $(git remote); do
+    git pull -r $remote main
+done
+
+printf "Pushing to all remotes..."
 # for remote in $(git remote); do
 #     git push $remote
 # done
 #
-# echo "Pushed to all remotes."
+printf "Pushed to all remotes."
 
